01497ed9bf0c026644520669fc4bf791b7f22701
Janusz Paprzycki <janusz.paprzycki@reckitt.com>
2023-10-26 17:40:22 +0200

    fix sprite flipping, add animations

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -3,8 +3,10 @@ import { Actor } from "./actor";
 
 export class Player extends Actor {
   constructor(scene: Scene) {
-    super(scene, 0, 60, "player");
+    super(scene, 16, 400, "player");
     this.setCollideWorldBounds(true);
+
+    this.initAnimations();
 
     return this;
   }
@@ -34,5 +36,37 @@ export class Player extends Actor {
     if (this.getBody().position.x + this.getBody().width / 2 > this.scene.cameras.main.midPoint.x) {
       this.scene.cameras.main.startFollow(this);
     }
+  }
+
+  private initAnimations(): void {
+    this.scene.anims.create({
+      key: "jump",
+      frames: this.scene.anims.generateFrameNames("player", {
+        prefix: "move-up-",
+        end: 3,
+      }),
+      frameRate: 4,
+      repeat: -1,
+    });
+
+    this.scene.anims.create({
+      key: "run",
+      frames: this.scene.anims.generateFrameNames("player", {
+        prefix: "move-right-",
+        end: 3,
+      }),
+      frameRate: 4,
+      repeat: -1,
+    });
+
+    this.scene.anims.create({
+      key: "idle",
+      frames: this.scene.anims.generateFrameNames("player", {
+        prefix: "stay-down-",
+        end: 1,
+      }),
+      frameRate: 4,
+      repeat: -1,
+    });
   }
 }
